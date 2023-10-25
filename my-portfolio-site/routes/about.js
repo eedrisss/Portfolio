a7eca180290d79fc7b29c69808524341bb83820f
IDRIs <imustapha30@yahoo.com>
2023-10-25 12:46:27 -0400

    nodemon package added for render.com

--- my-portfolio-site/routes/about.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// About Me Page
-router.get('/', function(req, res, next) {
-    res.render('about', { title: 'About Me' });
-});
-
-module.exports = router;
